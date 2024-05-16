517dc87c8f63f3169c1baa9b13fb1777465e747b
JiaqiCUI666 <jiaqicui666@qq.com>
2024-05-16 20:18:01 +0800

    利用IDE将修改提交到GitHub仓库

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,11 @@ import graph.Graph;
 import graph.GraphUI;
 import java.io.IOException;
 
-// 在 master 分支中进行操作
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         GraphUI gui = new GraphUI();
         gui.gui_start();
-        System.out.println("Hello World!");
     }
 }
