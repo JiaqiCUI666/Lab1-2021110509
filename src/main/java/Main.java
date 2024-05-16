6f42a47716b19dc9bdcefdba28b7dc228fc10d11
JiaqiCUI666 <jiaqicui666@qq.com>
2024-05-16 19:05:34 +0800

    提交

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,7 @@ import graph.Graph;
 import graph.GraphUI;
 import java.io.IOException;
 
-// 崔佳奇
-// 在这里增加一些注释
-// 软件工程实验一
+
 public class Main {
     public static void main(String[] args) throws IOException {
         GraphUI gui = new GraphUI();
